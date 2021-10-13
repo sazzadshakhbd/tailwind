73e9afbc000af4712eb72252af9f555a9ad42549
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-10-13 20:14:24 +0600

    create configuration file [tailwind.config.js]

---
+++ Modern-Landing-Page/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
