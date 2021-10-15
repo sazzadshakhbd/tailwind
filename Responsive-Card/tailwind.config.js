888879ebbb6fc1d445b7d0c2cefc010d4bf61738
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-10-15 21:03:41 +0600

    responisve card design

---
+++ Responsive-Card/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      padding: {
+        13: "3.25rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
